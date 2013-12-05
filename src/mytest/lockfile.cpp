067f20ba10df314a0de37ee15025ff3c6a57d662
wizard <wizard@myc.(none)>
2013-12-05 18:37:02 +0800

    删除编译的中间产生的中间文件

--- src/mytest/lockfile.cpp
+++ src/mytest/lockfile.cpp
@@ -4,23 +4,23 @@ LockFile::LockFile()
 {
 }
 
-LockFile *LockFile::instance()
-{
-    return lockFile();
-}
+//LockFile *LockFile::instance()
+//{
+//    return lockFile();
+//}
 
-bool LockFile::lock()
-{
-#defile LOCKMODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
-    int fd = open(fileName.toUtf8().data(), O_RDWR|O_CREAT, LOCKMODE);
-    if (fd < 0) {
-        qWarning("LockFile::lock: can not open %s for writing.", fileName.toUtf8().data());
-        return false;
-    }
+//bool LockFile::lock()
+//{
+//#defile LOCKMODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+//    int fd = open(fileName.toUtf8().data(), O_RDWR|O_CREAT, LOCKMODE);
+//    if (fd < 0) {
+//        qWarning("LockFile::lock: can not open %s for writing.", fileName.toUtf8().data());
+//        return false;
+//    }
 
-    if(lockfile(fd) < 0) {
-        return false;
-    }
+//    if(lockfile(fd) < 0) {
+//        return false;
+//    }
 
 
-}
+//}
